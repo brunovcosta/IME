dbfb1786b0ecd6e3dbbc204673d2a7e508521bbc
Bruno Vieira Costa <brunopsycon@gmail.com>
2016-12-11 15:41:45 -0200

    triggers wip

--- bd/estudo_vf/banco_imobiliario.sql
+++ bd/estudo_vf/banco_imobiliario.sql
@@ -8,6 +8,7 @@ create table regras.casas(
 	id integer default nextval('casas_seq') not null,
 	nome varchar(255) not null,--string de tamanho < 255
 	preco decimal(10,2) not null, --até 10 dígitos, 2 casas decimais
+	pontuacao integer not null,
 	constraint casas_pk primary key (id),
 	constraint casas_nome_uniq unique (nome)
 );
@@ -22,9 +23,14 @@ create table regras.casas_tabuleiros(
 	tabuleiro_id integer not null,
 	casa_id integer not null,
 	posicao integer not null,
-	constraint ct_pk primary key (casa_id,tabuleiro_id,posicao),
-	constraint ct_tabuleiros_fg foreign key (tabuleiro_id) references regras.tabuleiros (id),
+	constraint ct_pk primary key (tabuleiro_id,posicao),
+	constraint ct_tabuleiros_fg foreign key (tabuleiro_id) references regras.tabuleiros (id)
+		on delete cascade
+		on update cascade,
 	constraint ct_casas_fg foreign key (casa_id) references regras.casas (id)
+		on delete cascade
+		on update cascade,
+	constraint ct_posicao_positiva check(posicao>0)
 );
 
 create schema estados;
@@ -34,6 +40,7 @@ create table estados.jogadores(
 	nome varchar(255) not null,
 	login varchar(255) not null,
 	password_hash char(511) not null,
+	constraint jogador_login_uniq unique (login),
 	constraint jogadores_pk primary key (id)
 );
 create sequence jogos_seq;
@@ -43,11 +50,60 @@ create table estados.jogos(
 	tabuleiro_id integer not null,
 	constraint jogos_pk primary key (id),
 	constraint jt_fg foreign key (tabuleiro_id) references regras.tabuleiros (id)
+		on delete cascade
+		on update cascade
 );
-create table estados.jogadores_jogos(
+create table estados.jogadas(
 	jogo_id integer not null,
 	jogador_id integer not null,
+	posicao integer not null,
+	pontuacao integer default 0 not null,
+	dinheiro integer default 500 not null,
+	compra boolean default false not null,
 	constraint jj_pk primary key (jogo_id,jogador_id),
-	constraint jj_jogo_fg foreign key (jogo_id) references estados.jogos (id),
+	constraint jj_jogo_fg foreign key (jogo_id) references estados.jogos (id)
+		on delete cascade
+		on update cascade,
 	constraint jj_jogador_fg foreign key (jogador_id) references estados.jogadores (id)
+		on delete cascade
+		on update cascade
 );
+--TRIGGERS
+create function transacoes() returns trigger as
+' begin
+	if new.compra = true and new.dinheiro >= all (
+				select c.preco
+				from estados.jogos j
+				left join regras.casas_tabuleiros ct
+				on ct.tabuleiro_id = j.tabuleiro_id
+				left join regras.casas c
+				on c.id = ct.casa_id
+				where
+					ct.posicao = new.posicao
+			)
+	then
+		update estados.jogadas
+		set
+			dinheiro = dinheiro - (
+				select c.preco
+				from estados.jogos j
+				left join regras.casas_tabuleiros ct
+				on ct.tabuleiro_id = j.tabuleiro_id
+				left join regras.casas c
+				on c.id = ct.casa_id
+				where
+					ct.posicao = new.posicao
+			)
+		where
+			jogador.id = new.jogador_id and
+			jogo_id = new.jogo.id
+	end if;
+end;' language plpgsql; 
+create trigger ao_caminhar
+	before update
+	of posicao
+	on estados.jogadas
+	for each row
+	execute procedure transacoes();
+
+
