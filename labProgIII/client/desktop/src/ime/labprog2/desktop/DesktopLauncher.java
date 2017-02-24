e77cb8287f8edd1de4637b73f2685dd8cb4a5bb8
Bruno Vieira Costa <brunopsycon@gmail.com>
2017-02-24 12:47:37 -0300

    labProgIII

---
+++ labProgIII/client/desktop/src/ime/labprog2/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ime.labprog2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ime.labprog2.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
