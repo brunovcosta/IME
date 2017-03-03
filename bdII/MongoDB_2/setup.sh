0bd7b87bbb8d4595fcf06f9b87e36c8abd07216f
Bruno V. Costa <brunopsycon@gmail.com>
2017-03-03 08:21:56 -0300

    setup script created

---
+++ bdII/MongoDB_2/setup.sh
@@ -0,0 +1,16 @@
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+apt-get update
+apt-get install -y mongodb-org
+
+gedit /etc/systemd/system/mongodb.service
+
+systemctl start mongodb
+systemctl status mongodb
+systemctl enable mongodb
+
+./import2
+
+mongo
+
+
